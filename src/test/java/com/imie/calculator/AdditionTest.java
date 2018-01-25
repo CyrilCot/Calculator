948b07618d05f4f354af1d68bdb701ab7020286d
Cyril <cyril.cotance@imie.fr>
2018-01-25 14:35:32 +0100

    test

--- src/test/java/com/imie/calculator/AdditionTest.java
+++ src/test/java/com/imie/calculator/AdditionTest.java
@@ -3,13 +3,8 @@
  */
 package com.imie.calculator;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 
--- src/test/java/com/imie/calculator/DivideTest.java
+++ src/test/java/com/imie/calculator/DivideTest.java
@@ -1,7 +1,5 @@
 package com.imie.calculator;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
